109fbb774cd011b37ccc22c8dad1c7dc4838763e
ORHAN <baptiste.orhan@cgi.com>
2018-12-13 16:39:50 +0100

    Ajout de l'implémentation

---
+++ Pokedex/src/PokeManagement.java
@@ -0,0 +1,45 @@
+import java.util.List;
+
+public class PokeManagement implements PokeManager {
+
+    @Override
+    public void addPokemon(String Name, String Type, String Sex, String Environment, Integer Size, List Trainer, String Attack1, String Attack2, String Attack3, String Attack4) {
+
+
+    }
+
+    @Override
+    public void deletePokemon(String Name) {
+
+    }
+
+    @Override
+    public void addTrainer(String Name, String Sex, String City, Integer Age, Integer Size, List Pokemon) {
+
+    }
+
+    @Override
+    public void deleteTrainer(String Name) {
+
+    }
+
+    @Override
+    public void displayAllPokemon(List Pokemon) {
+
+    }
+
+    @Override
+    public void displayPokeDetails(String Name, String Type, String Sex, String Environment, Integer Size, List Trainer, String Attack1, String Attack2, String Attack3, String Attack4) {
+
+    }
+
+    @Override
+    public void displayAllTrainer(List Trainer) {
+
+    }
+
+    @Override
+    public void displayTrainerDetails(String Name, String Sex, String City, Integer Age, Integer Size, List Pokemon) {
+
+    }
+}
